7ec359bf2e08281778e0e0893babb23cd8b62378
mherrero347 <mherrero415@gmail.com>
2021-01-02 14:57:12 -0800

    Implements Venmo-tip-listening functionality proof of concept

--- tipjar.py
+++ tipjar.py
@@ -1,9 +1,37 @@
+from time import sleep
 from venmo_api import Client
 from secrets import access_token
 
 
+def light_up():
+    print("""
+ ___________  __    __       __      _____  ___   __   ___   ________       _______   ______     _______       ___________  __    __    _______      ___________  __       _______   ___    ___       ____  ____    
+("     _   ")/" |  | "\     /""\    (\"   \|"  \ |/"| /  ") /"       )     /"     "| /    " \   /"      \     ("     _   ")/" |  | "\  /"     "|    ("     _   ")|" \     |   __ "\ |"  |  |"  |     ))_ ")(  " \   
+ )__/  \\__/(:  (__)  :)   /    \   |.\\   \    |(: |/   / (:   \___/     (: ______)// ____  \ |:        |     )__/  \\__/(:  (__)  :)(: ______)     )__/  \\__/ ||  |    (. |__) :)||  |  ||  |    (____(  \__. \  
+    \\_ /    \/      \/   /' /\  \  |: \.   \\  ||    __/   \___  \        \/    | /  /    ) :)|_____/   )        \\_ /    \/      \/  \/    |          \\_ /    |:  |    |:  ____/ |:  |  |:  |     _____     ) :) 
+    |.  |    //  __  \\  //  __'  \ |.  \    \. |(// _  \    __/  \\       // ___)(: (____/ //  //      /         |.  |    //  __  \\  // ___)_         |.  |    |.  |    (|  /    _|  /  _|  /      ))_ ") __/ //  
+    \:  |   (:  (  )  :)/   /  \\  \|    \    \ ||: | \  \  /" \   :)     (:  (    \        /  |:  __   \         \:  |   (:  (  )  :)(:      "|        \:  |    /\  |\  /|__/ \  / |_/ )/ |_/ )    (____( /"   /   
+     \__|    \__|  |__/(___/    \___)\___|\____\)(__|  \__)(_______/       \__/     \"_____/   |__|  \___)         \__|    \__|  |__/  \_______)         \__|   (__\_|_)(_______)(_____/(_____/           (____/    
+""")
+
+
+def get_newest_transaction(venmo, me):
+    return venmo.user.get_user_transactions(user=me, count=1).pop()
+
+
 if __name__ == '__main__':
-    # Get your access token. You will need to complete the 2FA process
     venmo = Client(access_token=access_token)
 
-    
+    me = venmo.user.get_my_profile()
+    first_id = get_newest_transaction(venmo, me).id
+
+    while True:
+        newest = get_newest_transaction(venmo, me)
+
+        if newest.id == first_id or newest.payment_type != 'pay' or newest.target.id != me.id:
+            sleep(0.1)
+            continue
+
+        light_up()
+        print(vars(newest))
+        first_id = newest.id
