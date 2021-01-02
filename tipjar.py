b52e56a4551e976bb5f8c8260ddc8056f9972e84
mherrero347 <mherrero415@gmail.com>
2021-01-02 13:14:59 -0800

    Get Venmo API working

--- tipjar.py
+++ tipjar.py
@@ -1,2 +1,9 @@
+from venmo_api import Client
+from secrets import access_token
+
+
 if __name__ == '__main__':
-    print ("Caleb is a butt")
+    # Get your access token. You will need to complete the 2FA process
+    venmo = Client(access_token=access_token)
+
+    
